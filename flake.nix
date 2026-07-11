9bc1da25727b65ab50f098729933157338ae08ca
Teng Liu <teng@trnglina.org>
2026-07-11 13:14:13 +0900

    project: add rust

--- flake.nix
+++ flake.nix
@@ -2,18 +2,26 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
     {
       nixpkgs,
       flake-utils,
+      rust-overlay,
       ...
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+
+        rust = pkgs.rust-bin.stable.latest.default;
 
         swi-prolog = pkgs.stdenv.mkDerivation {
           name = "swi-prolog";
@@ -65,6 +73,7 @@
       {
         devShells.default = pkgs.mkShell {
           buildInputs = [
+            rust
             swi-prolog
           ];
         };
