f3f4b78ae3818e71c2f6bab658b1b4b25f5ee04b
Teng Liu <teng@trnglina.org>
2026-07-12 17:22:45 +0900

    native: expose dummy version predicate

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -1,1 +1,33 @@
+mod interop;
 
+use std::ffi::CString;
+use std::os::raw::c_void;
+
+use swi_fli::{PL_register_foreign_in_module, foreign_t, term_t};
+
+use interop::{FALSE, TRUE, guard, unify_atom};
+
+unsafe extern "C" fn pl_version(a0: term_t) -> foreign_t {
+    guard(|| {
+        if unify_atom(a0, env!("CARGO_PKG_VERSION")) {
+            TRUE
+        } else {
+            FALSE
+        }
+    })
+}
+
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn install_native() {
+    let module = CString::new("ui_native").unwrap();
+    unsafe {
+        register(&module, "version", 1, pl_version as *mut c_void);
+    }
+}
+
+unsafe fn register(module: &CString, name: &str, arity: i32, func: *mut c_void) {
+    let name = CString::new(name).unwrap();
+    unsafe {
+        PL_register_foreign_in_module(module.as_ptr(), name.as_ptr(), arity, func, 0);
+    }
+}
